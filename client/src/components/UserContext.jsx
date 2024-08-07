2574026c4d807eae6d58187f6732d5b481f8f712
ronakdevani15 <ronakdevani15@gmail.com>
2024-08-07 08:22:09 +0530

    Update UserContext.jsx

--- client/src/components/UserContext.jsx
+++ client/src/components/UserContext.jsx
@@ -9,7 +9,7 @@ export const UserContext = ({children}) => {
 
     const handleUser = async () => {
         try {
-          const res = await axios.get('https://authmern-f9y9.onrender.com//api/v1/current')
+          const res = await axios.get('https://authmern-f9y9.onrender.com/api/v1/current')
           setUserToggle(res.data.success)
         } catch (error) {
           console.log(error.message)        
